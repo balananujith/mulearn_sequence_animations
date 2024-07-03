828157acba1f0102d54396d8e50a5181635eea7b
balananujith <balananujith@gmail.com>
2024-07-03 17:07:44 +0530

    updated #2

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
+//Declare the variables 
+const item1=document.querySelector('#itemone');
+const item2=document.querySelector('#itemtwo');
+const item3=document.querySelector('#itemthree');
 
-const aliceTumbling = [
-    { transform: 'rotate(0) scale(1)' },
-    { transform: 'rotate(360deg) scale(0)' }
-  ];
-  
-  const aliceTiming = {
-    duration: 2000,
-    iterations: 1,
-    fill: 'forwards'
-  }
-  
-  const alice1 = document.querySelector("#alice1");
-  const alice2 = document.querySelector("#alice2");
-  const alice3 = document.querySelector("#alice3");
-  
+//animation doing function
+function animation(items,duration) {
+    items.animate([
+        { transform: 'rotate(0deg) scale(1)' },
+        { transform: 'rotate(360deg) scale(0)' }
+    ], {
+        duration: duration,
+        iterations: 1,
+        fill: 'forwards'
+    });
+}
+
+//animation excution function
+function startAnimation() {
+    const duration=2000;
+    
+    //first animation
+    animation(item1,duration)
+
+    //second animation
+    setTimeout(() => {
+        animation(item2,duration)
+    }, duration);
+
+    //Third animation
+    setTimeout(() => {
+        animation(item3,duration)
+    }, duration*2);
+
+}
+
+startAnimation()
